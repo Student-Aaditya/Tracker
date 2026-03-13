03270088b62dfaeacc23410a8058a0532e09ef8b
Student-Aaditya <152685023+Student-Aaditya@users.noreply.github.com>
2026-03-13 05:35:24 +0000

    addin mongoatals url

--- Tracking_Portal/utils/mongoose.js
+++ Tracking_Portal/utils/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/issue_tracker");
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("Mongoose connected successfully");
 
